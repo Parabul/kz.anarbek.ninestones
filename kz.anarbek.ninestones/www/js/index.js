bdd899da919371c14536d14b6417a76df59e115e
Parabul <iskaliev@gmail.com>
2015-04-19 01:52:37 +0600

    styles && steps Signed-off-by: Parabul <iskaliev@gmail.com>

--- kz.anarbek.ninestones/www/js/index.js
+++ kz.anarbek.ninestones/www/js/index.js
@@ -13,31 +13,115 @@
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
+ * specific language governing permissions && limitations
  * under the License.
  */
+ 
+	var FIRST=1;
+	var SECOND=2;
+ 
+function initBalls(){
+	var cnt=1;
+	for(var player=1;player<3;player++){	
+		for(var cell=1;cell<10;cell++){
+			for(var i=1;i<10;i++){
+				$('#cell-'+player+'-'+cell).append('<div class="ball" cell="'+cell+'" player="'+player+'" id="ball'+(cnt++)+'"></div>');
+			}
+		}
+	}
+}
+
+function getNextCell(cell,player){
+
+	if(player == FIRST && cell == 9){
+		return {player: SECOND,cell:1}
+    }else if (player == FIRST){
+        return {player: FIRST,cell:parseInt(cell)+1}
+    }else if (player == SECOND && cell == 9){
+        return {player:FIRST,cell:1}
+    }else if (player == SECOND){
+        return {player:SECOND,cell:parseInt(cell)+1}
+    }
+}
+
+function getNextCellObj(obj){
+	var player=obj.player;
+	var cell=obj.cell;
+	return getNextCell(cell,player);
+}
+
+function moveBallToCell(ball, cell){
+	ball.appendTo(getCellByInfo(cell));
+}
+
+function getCellInfo(self){
+	return {player:self.attr('player'),cell:self.attr('cell')}
+}
+
+function getCellByInfo(obj){
+	return $('#cell-'+(obj.player)+'-'+obj.cell);
+}
+
 function appReady(){
 		console.log('device ready');
+		initBalls();
+		
 		var width=$(window).width();
 		var height=$(window).height();
-		console.log({height:height,width:width});
-		$('#game-info').height(height/6);
+		
+		$('#game-info').height(height/6);	
+		$('.player-side').height(5*height/12-4);
 		$('#board').height(height);
 		$('#board').width(width);
-		ballDiameter=(width-10*3)/9/3;
-		$("#ball").height(ballDiameter);
-		$("#ball").width(ballDiameter);
-		$("#ball").css('border-radius',width);
 		
-		$('#ball').animate({
-				translate3d: (width-2-ballDiameter)+"px,"+(height-2-ballDiameter)+"px,0"
-			}, 2000);
+		
+		ballDiameter=(width-10*3)/9/4-1;
+		balls=$(".ball");
+		balls.height(ballDiameter);
+		balls.width(ballDiameter);
+		balls.css('border-radius',ballDiameter);
+
+		//$('.ball').on('click', function() {
+		//	$(this).appendTo('#cell-1-3');
+		//}); 
+		
+		$('.cell').on('click', function() {
+			var stepCell=$(this);
+			var stepCellInfo=getCellInfo($(this));
 			
-		$('#ball').on('click', function() {
-			$('#ball').animate({				
-				translate3d: "2px,2px,0"
-			}, 2000);
+			console.log($(this).attr('cell'));
+			cellBalls=$(this).children('.ball');
+			handSize=cellBalls.length;
+			var nextCell;
+			var currentCell=getCellInfo($(this));
+			var cnt=0;
+			cellBalls.each(function(){
+				cnt++;
+				if(handSize==1)
+					currentCell=getNextCellObj(currentCell);
+				
+				ball=$(this);
+				moveBallToCell(ball,currentCell);
+				
+				
+				if(cnt==handSize){	
+					console.log('finalCell');
+					
+					var finalCell=getCellByInfo(currentCell);
+					var finalCellInfo=getCellInfo(finalCell);
+					console.log(finalCell);
+					var finalCellBalls=finalCell.children('.ball');
+					console.log(finalCellBalls.length);
+					if((finalCellBalls.length %2 == 0) && (stepCellInfo.player!=finalCellInfo.player)){	
+						finalCellBalls.hide();
+					}
+					
+				}
+				currentCell=getNextCellObj(currentCell);
+			});
+			
 		}); 
+		
 }
 document.addEventListener("deviceready", appReady, false);
 
